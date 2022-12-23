37f072d42325b213c60895ab5dfa3c7c5cc3ebcc
JC <jchan48@gmail.com>
2022-12-23 12:32:38 -0500

    updated static files

---
+++ app.js
@@ -0,0 +1,30 @@
+const scriptURL = process.env.scriptURL || "/sw.js";
+const scope = process.env.scope || '/';
+
+/**
+ * Should only work for https... and hopefully localhost
+ * @return {Promise<void>}
+ */
+const registerServiceWorker = async () => {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register(scriptURL, {
+        // scope: "/",
+        scope,
+      });
+      if (registration.installing) {
+        console.log("Service worker installing");
+      } else if (registration.waiting) {
+        console.log("Service worker installed");
+      } else if (registration.active) {
+        console.log("Service worker active");
+      }
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
+  }
+};
+
+// …
+
+registerServiceWorker();
